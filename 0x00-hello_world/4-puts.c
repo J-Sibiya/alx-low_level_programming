ff297cbee6f7ff961d79126eb9959c3b48b37b77
J-Sibiya <j.sibiya365@gmail.com>
2022-09-09 16:19:16 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
+vim 4-puts.c
+
 #include <stdio.h>
 
 /**
- */ main-Printing
-*
-* Return: Always 0 (success)
-* /
-* int main(void)
-	* {
-		* puts("\"Programming is like building a multilingual puzzle");
-		* return (0)
-			*} 
+ * main - Printing
+ *
+ * Return:Always o(Successful)
+ * /
+ int main(void)
+ {
+      puts("\"Programming is like building a multilingual puzzle");return (0) 
+      }
